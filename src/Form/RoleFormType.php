e45b3a553459745cb9487a7bb90c383459bc9747
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2024-11-11 18:30:52 +0100

    :hammer: add RoleFormType class

---
+++ src/Form/RoleFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\Form;
+use App\Entity\Role;
+class RoleFormType
+{
+    public static function buildForm(?Role $role = null): Form
+    {
+        $form = new Form();
+        $form
+            ->addField
+            (
+                'name',
+                'text',
+                'name',
+                $role ? $role->getName() : '',
+                [
+                    'required' => true,
+                    'placeholder' => $role ? '' : 'Membre'
+                ]
+            )
+            ->addField
+            (
+                'submit',
+                'submit',
+                'submit',
+                'Submit'
+            )
+            ;
+        return $form;
+    }
+}
