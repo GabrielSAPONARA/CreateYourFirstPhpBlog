079528626a2a2eb5f436f6b8e3642b9968159548
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2024-11-11 18:23:49 +0100

    :hammer: create form class

---
+++ src/Form/Form.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+class Form
+{
+    private $fields = [];
+    private $data = [];
+    private $errors = [];
+
+    public function getFields()
+    {
+        return $this->fields;
+    }
+    public function addField($name, $type, $id, $value, $options = [])
+    {
+        $this->fields[$name] =
+            [
+                'type' => $type,
+                'id' => $id,
+                'value' => $value,
+                'options' => $options
+            ];
+
+        return $this;
+    }
+
+    public function bind(array $data)
+    {
+        $this->data = $data;
+        foreach ($this->fields as $name => $field) {
+            if (isset($data[$name])) {
+                $this->data[$name] = $data[$name];
+            } else {
+                $this->data[$name] = null;
+            }
+        }
+    }
+
+    public function validate()
+    {
+        foreach ($this->fields as $name => $field) {
+            if (isset($field['options']['required']) && $field['options']['required'] && empty($this->data[$name])) {
+                $this->errors[$name] = 'Ce champ est requis.';
+            }
+            // Ajouter d'autres règles de validation ici
+        }
+    }
+
+    public function isValid()
+    {
+        return empty($this->errors);
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
