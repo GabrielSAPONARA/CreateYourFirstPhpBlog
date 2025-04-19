87272244707cacefdfe5c0003d99b67ab550471d
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2025-04-19 14:00:16 +0200

    :bug: fix user access and profile update

---
+++ src/Form/UserRoleFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\Form\Form;
+use App\Form\Type\RoleType;
+use App\Form\Type\SubmitType;
+
+class UserRoleFormType
+{
+    public static function buildForm(?User $user = null, array $roles = []): Form
+    {
+        $form = new Form();
+
+        RoleType::addField($form, $user, $roles);
+
+        SubmitType::addField($form, 'Submit');
+
+        return $form;
+    }
+}
